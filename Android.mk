bb6ccd82397f8b7b0f955557ba809192a48bbc15
sbwml <984419930@qq.com>
2020-11-26 09:40:49 +0800

    mata: Update Graphics and Vulkan blobs from taimen
    * Android R
    * OpenGL ES 3.2 V@0490.0
    * Vulkan 1.1.128
    
    Change-Id: I4432bf139d47fb02a10d7a4710b6bafa1b71ed8b

--- Android.mk
+++ Android.mk
@@ -46,6 +46,23 @@ $(DSP_RECOVERY_MOUNT_POINT):
 
 ALL_DEFAULT_INSTALLED_MODULES += $(DSP_RECOVERY_MOUNT_POINT)
 
+EGL_LIBS := eglSubDriverAndroid.so libEGL_adreno.so libGLESv1_CM_adreno.so libGLESv2_adreno.so libq3dtools_adreno.so libq3dtools_esx.so
+EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(EGL_LIBS)))
+$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(notdir $(EGL_LIBS)))
+$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
+
 FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
 $(FIRMWARE_MOUNT_POINT):
 	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
