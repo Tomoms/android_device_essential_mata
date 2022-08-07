d008b72292608b613a095a56c67331677c927ff2
Tommaso Fonda <fondatommaso2@gmail.com>
2022-08-07 10:30:11 +0200

    mata: Build F-DroidPrivilegedExtension
    Change-Id: I2a2521e77d3d61070d70c54ac9edbc056ba488f6

--- device.mk
+++ device.mk
@@ -144,6 +144,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
+# F-Droid
+PRODUCT_PACKAGES += \
+    F-DroidPrivilegedExtension
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
