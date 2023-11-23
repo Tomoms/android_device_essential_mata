9ddbdbd30cff783ecaba846cb5c021d2797637f5
Zhoneym <140673973+Zhoneym@users.noreply.github.com>
2023-11-23 16:27:49 +0800

    mata: Build libprotobuf-cpp-full-3.9.1-vendorcompat

--- device.mk
+++ device.mk
@@ -353,8 +353,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.0.vendor \
     android.hardware.radio.config@1.0.vendor \
-    android.hardware.secure_element@1.2.vendor \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat
+    android.hardware.secure_element@1.2.vendor 
 
 # Seccomp
 PRODUCT_COPY_FILES += \
