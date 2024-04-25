eaf14aed1e84c45c4898b88f0f06db6a39bdeb99
Tommaso Fonda <fondatommaso2@gmail.com>
2024-04-25 13:09:27 +0200

    Revert "mata: Migrate to Health 2.1 HAL"
    This reverts commit 6c32cd177200fbb438b5bbe71a5777bf87d6e11e.

--- device.mk
+++ device.mk
@@ -190,9 +190,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-service.mata
 
 # HDR
 PRODUCT_COPY_FILES += \
