1cec1d6d07e65df59b17606130d67e55ea4cf5f5
Tommaso Fonda <fondatommaso2@gmail.com>
2025-09-04 18:58:28 +0200

    mata: Disable UFFD GC via OVERRIDE_ENABLE_UFFD_GC
    * Our kernel (4.4) does not support features needed for UFFD GC.
      Leaving it enabled results in long waiting time on the first boot.
    
    Change-Id: Ia8a17467e13d343c03ee29e514994946e7e84985

--- device.mk
+++ device.mk
@@ -388,6 +388,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
